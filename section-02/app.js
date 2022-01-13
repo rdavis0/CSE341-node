6a8b4b52b7b487a1262d40c98acec0a5fe7da662
rdavis0 <rdfiasco@gmail.com>
2022-01-12 17:02:44 -0700

    starting templates

--- section-02/app.js
+++ section-02/app.js
@@ -4,15 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser'); //npm install --save body-parser
 const app = express(); 
 
+app.set('view engine', 'pug');
+app.set('views', 'views'); // file path to templates
+
 const rootDir = require('./util/path.js');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
