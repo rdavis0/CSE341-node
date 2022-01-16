55b4e004aadef1786686a7c34299a74b7b9e0d07
rdavis0 <rdfiasco@gmail.com>
2022-01-15 18:14:51 -0700

    section 02 cont

--- section-02/app.js
+++ section-02/app.js
@@ -2,9 +2,19 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser'); //npm install --save body-parser
+const expressHbs = require('express-handlebars');
+
 const app = express(); 
 
-app.set('view engine', 'pug');
+app.engine(
+    'hbs', 
+    expressHbs({
+        layoutsDir: 'views/layouts/', 
+        defaultLayout: 'main-layout',
+        extname: 'hbs'
+    })
+);
+app.set('view engine', 'hbs');
 app.set('views', 'views'); // file path to templates
 
 const rootDir = require('./util/path.js');
@@ -19,7 +29,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-})
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
 app.listen(3000);
