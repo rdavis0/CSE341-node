394b652bf33ed7e0ed4a61d3e5db97945892a890
rdavis0 <rdfiasco+github@gmail.com>
2022-01-31 22:07:50 -0700

    section 12 first half

--- section-12/util/database.js
+++ section-12/util/database.js
@@ -1,8 +1,25 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://rdavis:mvxezSmZb1wSWUR1@cluster0.pbeyn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected to MongoDB');
+            _db = client.db();
+            callback(client);
+        }).catch(err => {
+            console.log(err);
+            throw err;
+        });
+};
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'Error: No database found';
+}
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
