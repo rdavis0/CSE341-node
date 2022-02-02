891a45a77d85bb0fe8d587b937ff1e24ff0b04d3
rdavis0 <rdfiasco+github@gmail.com>
2022-02-01 21:57:02 -0700

    section 12

--- section-12/models/user.js
+++ section-12/models/user.js
@@ -1,16 +1,36 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User {
+    constructor(username, email) {
+        this.name = username;
+        this.email = email;
+    }
+
+    save() {
+        const db = getDb();
+        db.collection('users').insertOne(this)
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users').findOne({ _id: new ObjectId(userId) })
+            .then(result => {
+                console.log(result);
+                return result;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+}
 
 module.exports = User;
