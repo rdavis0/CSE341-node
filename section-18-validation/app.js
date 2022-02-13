39686f6fb34dc7bc012e381225560419e0abb84c
rdavis0 <rdfiasco+github@gmail.com>
2022-02-12 23:53:42 -0700

    section 19 + week 6 completed

--- section-18-validation/app.js
+++ section-18-validation/app.js
@@ -57,15 +57,10 @@ app.use((req, res, next) => {
     }
     User.findById(req.session.user._id)
         .then(user => {
-            if (!user) {
-                return next();
-            }
             req.user = user;
             next();
         })
-        .catch(err => {
-            next(new Error(err));
-        });
+        .catch(err => console.log(err));
 });
 
 app.use((req, res, next) => {
@@ -78,13 +73,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.get('/500', errorController.get500);
-
 app.use(errorController.get404);
-
-app.use((error, req, res, next) => {
-    res.redirect('/500');
-});
 
 const corsOptions = {
     origin: "https://cse341-rd.herokuapp.com/",
